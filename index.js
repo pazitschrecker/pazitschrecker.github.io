b4189474fa4140731be6681c2a670785b66ead91
Pazit <pazitrose1999@gmail.com>
2020-12-03 09:10:06 -0500

    Created layout

---
+++ index.js
@@ -0,0 +1,36 @@
+function removeActive() {
+  $("#nav-about").removeClass("active-item");
+  $("#nav-resume").removeClass("active-item");
+  $("#nav-projects").removeClass("active-item");
+  $("#nav-contact").removeClass("active-item");
+}
+
+$(document).ready(function () {
+  $(".nav-link").click(function () {
+    console.log("hello");
+  });
+
+  $("#get-in-touch").click(function () {
+    console.log("hello");
+  });
+
+  $("#nav-about").click(function () {
+    removeActive();
+    $(this).addClass("active-item");
+  });
+
+  $("#nav-resume").click(function () {
+    removeActive();
+    $(this).addClass("active-item");
+  });
+
+  $("#nav-projects").click(function () {
+    removeActive();
+    $(this).addClass("active-item");
+  });
+
+  $("#nav-contact").click(function () {
+    removeActive();
+    $(this).addClass("active-item");
+  });
+});
